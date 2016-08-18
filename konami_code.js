cdb886efe18e5799e429898fe981307ba54bab43
Peter Tascio <peter.tascio@gmail.com>
2016-08-18 14:32:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var i = 0;
+var count = 0;
 function init() {
   // your code here
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', function(e){
+    //this code below is so I could see what I was typing, not necessary for actual function
+    // var pgh = document.createElement('p');
+    // pgh.appendChild(document.createTextNode(e.which));
+    // body.appendChild(pgh);
+   if (e.which === code[i]){
+     count+=1;
+     if (count === 10){
+       alert("You've cracked the code!");
+     }
+     i+=1;
+  }else{
+    i = 0;
+    count = 0;
+  }
+  })
 }
+
+
+
+
+// const input = document.querySelector('input')
+//
+// input.addEventListener('keydown', function(e) {
+//   console.log(e.which)
+// })
