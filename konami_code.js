6c1e30a8732cd672d2e1e86a038e2502f5c9934b
Cindy Gao <cindygao@hotmail.com>
2016-08-30 19:08:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
-  const body = document.querySelector('body')
+  //const body = document.querySelector('body')
 
   var index = 0
-  body.addEventListener('keydown', function(e){
+  document.querySelector('body').addEventListener('keydown', function(e){
     if(parseInt(e.which) === code[index]){
       index++
       if(index === code.length-1){
