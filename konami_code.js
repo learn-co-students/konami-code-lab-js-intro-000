376ccacd65ef2f803815d3e877116fdd42b5ea1e
Cindy Gao <cindygao@hotmail.com>
2016-08-30 19:54:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
 
   var index = 0
   document.querySelector('body').addEventListener('keydown', function(e){
-    if(parseInt(e.which) === code[index]){
+    if(parseInt(e.which || e.detail) === code[index]){
       index++
       if(index === code.length-1){
         alert("YOU DID IT!")
