15b0b3c2f9b18f272b9f4ffe05b367c0b70a774d
Danny Marquez <dannybrianmarquez@gmail.com>
2016-09-12 15:58:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+
+  var input = [];
+
+  var $ = document.body;
+  $.addEventListener('keydown', function(e) {
+    input.push(e.which || e.detail);
+
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] != code[i]) {
+        input = [];
+        break;
+      }
+      else if (input.length === code.length) {
+        alert("You did it! 30 lives for you!!");
+        break;
+      }
+    }
+  });
 }
