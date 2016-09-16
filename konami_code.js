ee722bfca007a7be3c124234ce06a7cea70deeb3
Jesse Bryan <jbryanflatiron@gmail.com>
2016-09-16 19:27:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', check)
 }
+
+function check(e){
+  if(parseInt(e.detail || e.which) === code[index]) {
+    index++;
+
+    if(index === code.length) {
+      alert("congrats lol");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+init();
