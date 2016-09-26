0c7f5b5fd812fe704d6fe8f9329d371c26a7cf21
Christiane Heiligers <tina.heiligers@gmail.com>
2016-09-26 17:55:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
   // your code here
+  var codeIndex = 0 //keep track of index outside of event handler
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+      if (key === code[codeIndex]) {
+        codeIndex++
+
+        if (codeIndex === code.length - 1) {
+          alert('Correct Code Entered')
+          codeIndex = 0
+        }
+        else {
+          index = 0
+        }
+      }
+  })
 }
