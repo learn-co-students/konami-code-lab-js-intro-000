fb191466e6dacaf5620531248f7bcafce8f65c56
MoJen Jenkins <mojen@thenoisemakers.co.uk>
2016-10-13 02:40:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+
+  const body = document.querySelector('body');
+
+
+  var inputtedCode = [];
+
+  body.addEventListener( 'keydown', function( checkCode ) {
+    inputtedCode.push( checkCode.detail );
+    if ( inputtedCode.toString() === code.toString() ) {
+      alert( 'CODE ENTERED. WORLD UNLOCKED');
+    }
+  })
+
 }
