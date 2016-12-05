f6e5b2dae47b825a11d05cfb0c10fde06140556d
Travis Payne <thpaggie910@gmail.com>
2016-12-05 03:02:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    let keyPressed = parseInt(e.which || e.detail)
+    if(keyPressed === code[index]){
+      index++
+
+      if(index === code.length - 1) {
+        alert("You now have infinite lives!!!")
+      }
+    } else {
+      index = 0
+    }
+  });
 }
+
+init()
