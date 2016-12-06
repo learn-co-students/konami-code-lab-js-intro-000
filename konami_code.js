535484eda0f0a8d4da70bc849b09ccc42876f0e4
Zachary Slates <zach.slates@gmail.com>
2016-12-06 02:37:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,30 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var tenLastKeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
   // your code here
+function init(){
+//to create a rolling array of the last 10 keystroke values
+//To compare the values at [i] of two arrays
+  function compareArrays (array1, array2){
+    var same = true;
+    for(var i = 0; i < 10; i++){
+      if (same === false){
+        return false;
+      }else if(array1[i] === array2[i]){
+        same = true;
+      }else{
+        same = false;
+      }
+    }
+    return true;
+  }
+
+  document.body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+    tenLastKeys.push(key);
+    tenLastKeys.shift();
+    if (compareArrays(code, tenLastKeys) === true){
+        alert("Congrats! You found the Easter Egg!");
+    }
+  })
 }
