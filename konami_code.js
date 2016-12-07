1c01309a85669a0ea4e30874b1cb938b4547a1a2
Sam Comerford <samcomerford1@Sams-MBP.home>
2016-12-06 22:50:19 -0500

    updated

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
-  // your code here
+
+var index = 0
+
+document.addEventListener('keydown', function (e) {
+
+  const key = parseInt(e.detail || e.which)
+
+  if (key === code[index]){
+    index++
+
+    if (index === code.length - 1) {
+      alert("WOO!")
+    }
+  } else {
+    index = 0
+  }
+ })
 }
+
+init()
