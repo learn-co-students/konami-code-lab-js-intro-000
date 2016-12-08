f7e686fa506628efd4e7586c3d6b01caa682d9fb
arturusfury <bri.reynolds@gmail.com>
2016-12-08 08:56:34 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var nextPosition = 0;
 
 function init() {
-  // your code here
+  document.addEventListener('keydown', checkCode);
 }
+
+function checkCode(e) {
+  const keyWhich = e.which;
+  const keyDetail = e.detail;
+  const nextKey = code[nextPosition];
+
+  if (keyWhich == nextKey || keyDetail == nextKey) {
+    nextPosition += 1
+
+    if (nextPosition == code.length) {
+      removeListener();
+      alert("Winner!");
+    }
+
+  } else {
+    nextPosition = 0;
+  }
+}
+
+function removeListener() {
+  document.removeEventListener('keydown', checkCode);
+}
+
+
+init()
