1f04f09f91a8d341d18e186b39367f4379513b81
Joshua Zuo <neveragain8209@yahoo.com>
2016-12-19 14:40:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // your code here
+var user = []
+document.body.addEventListener('keydown', init, false)
+function init(e) {
+    user.push(e.keyCode)
+    for(var i = 0; i<user.length; i++){
+        if(code[i] !== user[i]){
+            user = [];
+        }
+    }
+    if(code.length === user.length){
+        alert('YOU DID IT');
+    }
 }
