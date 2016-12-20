c8bf0e86c95210e265b1dadd89702bfb4bca4c89
Joshua Zuo <neveragain8209@yahoo.com>
2016-12-20 00:41:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var user = []
+var user = 0
 function init() {
-  document.body.addEventListener('keydown', entering, false)
-  function entering(e){
-    user.push(e.keyCode)
-    for(var i = 0; i<user.length; i++){
-        if(code[i] !== user[i]){
-            user = [];
-        }
+document.body.addEventListener('keydown', onKeyDownHandler, false)
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === code[user]) {
+    user++
+
+    if (user === code.length - 1) {
+      alert("YOU DID IT!")
+      user = 0
     }
-    if(code.length === user.length){
-        alert('YOU DID IT');
-    }
- }
+  } else {
+    user = 0
+  }
+}
 }
