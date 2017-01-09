e0cbea54cd9368ac7d2c868fafd2554504a3c635
Michael Lee <mleenavy@gmail.com>
2017-01-09 13:11:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = 0
+  const body = document.querySelector('body')
+  var onKeyDownHandler = (e) => {
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
+      index++
+      if (index === code.length - 1) {
+        alert("KONAMI!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+  body.addEventListener('keydown', onKeyDownHandler)
 }
