fee834a1a18b711b4fdf1824975c7fa8dc455122
Usman Pervaiz <de.usman@yahoo.com>
2017-01-27 19:53:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,21 @@
-var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var index = 0;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+function init() {
+  var index = 0
 
-document.body.addEventListener('keydown', init);
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail)
 
-function init(event) {
-  var key = parseInt(event.detail) ;
-    if(key === code[index]) {
-    index ++;
+    if (code[index] === key) {
+      index++
 
-    if(index === code.length) {
-    alert("YOU DID IT!");
-    index = 0;
+      if (index === code.length - 1) {
+        alert('hurray!')
+
+        index = 0
+      }
+    } else {
+      index = 0
     }
-    }
-    else {
-    index = 0;
-    }
+  })
 }
