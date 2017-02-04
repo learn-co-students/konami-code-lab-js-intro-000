c1764ec3d8a52d5ffe43b5b37ca363e3f0b2283f
Chuks Opia <chuks24ng@yahoo.co.uk>
2017-02-04 10:10:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  function keyHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (code.indexOf(key) !== -1) {
+      alert("YOU DID IT");
+    } else {
+      return e.preventDefault();
+    }
+  }
+  document.body.addEventListener('keydown', keyHandler);
 }
+
+init();
