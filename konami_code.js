7c604f9c402dc4da0889374f023729b88f4af81b
Helene Sofie Borthen Stenstadvold <helenesofies@gmail.com>
2017-02-17 15:49:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+index = 0;
+
+
 function init() {
-  // your code here
+  index = 0;
 }
+
+function listener(e){
+  const key = parseInt(e.detail || e.which)
+
+  if(key === code[index]){
+    if(index === code.length - 1){
+      alert("YOU DID IT!")
+      init();
+    return;
+    }
+  index++
+  }else {
+    init();
+  }
+}
+
+document.body.addEventListener('keydown', listener)
+init();
