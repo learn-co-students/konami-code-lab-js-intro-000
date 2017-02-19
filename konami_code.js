7c511c1d07280b6394e19ede83062761bb252525
Mike McNeeley <mmcneeley@gmail.com>
2017-02-19 06:45:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,47 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
 
 function init() {
-  // your code here
+
+  function eval(e) {
+    //console.log(`The value of index is ${index}`)
+    //console.log(`The value passed was ${(e.detail || e.which)}`)
+    const number = parseInt(e.detail || e.which)
+    if (number === code[index]) {
+        index++
+        if (index === code.length) {
+          //console.log('Success!')
+          alert("Success!")
+          index = 0
+        }
+    }
+    else {
+      //console.error('Try again')
+      //alert('Wrong Code')
+      index = 0
+    }
+  }
+
+  document.body.addEventListener('keydown', eval)
+
 }
+
+/*
+  window.addEventListener('keydown', function(e) {
+  var number = parseInt(e.detail || e.which)
+  if (code.indexOf(number) >= 0) {
+    console.log(number)
+    if (number === code[index]) {
+        index++
+    }
+    if (index === code.length) {
+      console.log("Success!")
+      index = 0
+    }
+  }
+  else {
+    console.error("Try again")
+    index = 0
+  }
+  })
+*/
