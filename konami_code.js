6fae58aa31bfc9119a04416e31481d66bb545783
Yujun Sung <yujunsung@gmail.com>
2017-02-28 02:36:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
-}
+function init(){
+
+  var index = 0;
+
+  document.addEventListener('keydown'||'click', function(e){
+    const key = parseInt(e.detail ||e.which);
+    var requiredKey = code[index];
+    if (key == requiredKey){
+      index = index +1;
+      if (index == code.length)
+      {
+          alert("You did it");
+          index =0;}
+
+    }else{
+      index =0;
+    }
+
+  });
+};
