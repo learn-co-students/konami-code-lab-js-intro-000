03611df6b3907f8c1f44ce21063b9b7a7ef1fea1
Eric Sperling <erno.goldfinger@gmail.com>
2017-02-28 12:08:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+var next = 0;
+
+function kcode(e) {
+  if (parseInt(e.detail || e.which) === code[next]) {
+    next++;
+  }
+  else {
+    next = 0;
+  }
+
+  if (next === code.length) {
+    alert("Yep.");
+  }
+}
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', kcode);
 }
