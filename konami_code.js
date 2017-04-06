14274338a295270ef3e6701939e1eaf1f749bef1
Andrew Ford <ajoeford@gmail.com>
2017-04-06 01:48:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var dupe = code.slice();
+
+  //set event listener for keydown
+  document.body.addEventListener('keydown',codeFunc);
+
+  function codeFunc(event){
+    walkCode(event);
+  }
+
+  function walkCode(event){
+    var garbageparse = parseInt(event.which || event.detail);
+    if(garbageparse===dupe[0]){
+      dupe.shift();
+      if(dupe.length===0){
+        alert('AWESOME POWER UNLOCKED!!!');
+        dupe = code.slice();
+      }
+    }
+    else{
+      dupe = code.slice();
+    }
+  }
 }
