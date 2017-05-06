c3885ea554f07d5d5ee24ae886c23ccc35d26181
Mike Erb <merb92@gmail.com>
2017-05-06 00:19:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,30 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  const body = document.querySelector('body')
+
+  var codePosition = 0
+
+  const lastPos = code.length - 1
+
+  function konamiCheck(e)
+  {
+    let keyPressed = parseInt(e.detail || e.which)
+    if (keyPressed === code[codePosition]) {
+      if (codePosition === lastPos) {
+        alert('happy day');
+        codePosition = 0
+      }
+      else {
+        codePosition ++
+      }
+    }
+    else {
+      codePosition = 0
+    }
+  }
+
+  body.addEventListener ('keydown', konamiCheck)
 }
+
+init()
