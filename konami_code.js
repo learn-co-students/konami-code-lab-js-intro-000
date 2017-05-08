0b0d405770d5cc3450d0f58400dfe8f70bf03c82
Uzor Jon-Ubabuco <uubabukoh@gmail.com>
2017-05-08 22:53:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', onKeyDownHandler);
+}
+
+
+function onKeyDownHandler(e) {
+  var key = parseInt(e.detail || e.which)
+
+  if (key === code[index]) {
+    index++
+    if (index === code.length) {
+      alert("Konami Code Confirmed, Sending Supplies")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
