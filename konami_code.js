6ba25f9cb2bfb9371903ef34895918cd4dfa5094
Lidia Starshinova <lida_st@list.ru>
2017-05-14 13:02:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', keyDownHandler)
+ var count = 0
+  function keyDownHandler(e){
+    var key = parseInt(e.detail || e.which)
+	if (key == code[count]){
+		count++
+	}
+	else {
+		count =0;
+	}
+	//console.log(count)
+	if (count== code.length){
+		//console.log ("you are")
+    alert("Congrats")
+	}
+
+  }
 }
+init()
