256f4d10071b7edcc85db4d3dca09ff54207bf2d
Hoi Ki Yuen <cyhk116@gmail.com>
2017-06-26 14:15:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var i = 0
 
 function init() {
+  var i = 0
   var handler = function onKeyDownHandler(e)
   {
     const key = parseInt(e.detail || e.which)
-    console.log(key);
     if (key === code[i]) i++;
     else i = 0;
 
