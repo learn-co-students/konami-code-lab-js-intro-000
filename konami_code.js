02b71d6f930d8c9cb1bd8f065edeb499c1d77b48
Alexander Marks-Katz Marks-Katz <english3000.org@gmail.com>
2017-07-04 17:23:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var keystrokes = []
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(o){
+    keystrokes.push(o.which)
+    if keystrokes == code
+      alert("That's the code! Congrats")
+  })
 }
