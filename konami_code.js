b6c88b2cf26a73e03944e6c9a834848f1b7590f3
sumanth Jesta <sumanthjesta14@gmail.com>
2017-07-23 14:02:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 
 function init(e) {
+  document.body.addEventListener("keydown",function(){
+
   let index = 0;
 
 // This is the function that would be invoked by the event listener.
@@ -23,7 +25,5 @@ function init(e) {
   } else {
     index = 0;
   }
+};
 }
-
-
-document.body.addEventListener("keydown",init(event));
