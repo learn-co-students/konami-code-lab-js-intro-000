272119139cdc3f4f4317c4bb949e7a757dfc9e6a
sumanth Jesta <sumanthjesta14@gmail.com>
2017-07-23 13:55:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+
+function init(e) {
+  let index = 0;
+
+// This is the function that would be invoked by the event listener.
+
+  const key = parseInt(e.detail);
+
+
+  if (key === code[index])
+  {
+    index++;
+
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+
+document.body.addEventListener("keydown",init(event));
