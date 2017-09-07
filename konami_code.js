b9d75fd9e65f86f4876a8ec731460c5dc00d381d
maulana rahman <maulana.auliar@gmail.com>
2017-09-07 18:15:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  var index = 0;
+  function onKeyDownHandler(e) {
+    var key_int = parseInt(e.which||e.detail);
+    if (key_int === code[index]) {
+      index++;
+      if (index === code.length) {
+        window.alert('CODE ACCEPTED!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
