ba413edb821b66e743e74bb6da82f7cb7837b3e6
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-12 11:41:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A
+var index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var body = document.querySelector('body');
+  body.addEventListener('keydown', function(e){
 
+  console.log(e.which); //show key
+
+  const key = parseInt(e.detail || e.which);
+  if (key === code[index]){ //compare key to code[index], first key===code[0]...
+    index++;
+
+      if (index === code.length){ //10 === 10
+        alert("Wohooo!!!");
+        index = 0;  //restart
+      }
+  }
+  else {
+    alert("WRONG");
+    index = 0;  //restart
+  }
+});
 }
