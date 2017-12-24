b1a65798f280ed75e6cb968331c3e1ed6ff4e537
Chris Albanese <chris@reaorlando.com>
2017-12-24 05:33:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,40 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+// function init() {
+// var i = 0
+//   document.body.addEventListener('keydown', function(e)) {
+//
+//     const key = parseInt(e.which || e.detail);
+//
+//     if (code[i] === key) {
+//       i++;
+//
+//       if (i === code.length) {
+//         alert('hurray!');
+//         i = 0;
+//       }
+//     } else {
+//       i = 0;
+//     }
+//   });
+// }
 
+function init() {
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+    console.log(key);
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
