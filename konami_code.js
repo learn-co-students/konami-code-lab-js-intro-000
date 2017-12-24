aa9b5b966a9b715a89be779def58de448d8abcb3
Chris Albanese <chris@reaorlando.com>
2017-12-24 05:44:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,30 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-// function init() {
-// var i = 0
-//   document.body.addEventListener('keydown', function(e)) {
-//
-//     const key = parseInt(e.which || e.detail);
-//
-//     if (code[i] === key) {
-//       i++;
-//
-//       if (i === code.length) {
-//         alert('hurray!');
-//         i = 0;
-//       }
-//     } else {
-//       i = 0;
-//     }
-//   });
-// }
-
 function init() {
   let index = 0;
 
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.which || e.detail);
-    console.log(key);
+    console.log(key, index);
     if (code[index] === key) {
       index++;
 
