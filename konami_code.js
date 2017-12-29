e41c5916009b68282487b14f16d45ad38a15c0bc
Cory West-Forbes <cwestforbes@gmail.com>
2017-12-29 20:25:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(e.which);
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length) {
+      alert('Godmode achieved!');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const input = document.querySelector('body');
+  input.addEventListener('keydown', onKeyDownHandler);
 }
+
+init();
