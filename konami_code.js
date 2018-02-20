6b62b5d9badd920a45e02d621e57c77c6a1325cc
Alex Segers <a.segers.dev@gmail.com>
2018-02-20 23:50:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,21 @@
-//const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const code = [65, 76, 69, 88]
-var index = 0;
-function init(e) {
-  const key = parseInt(e.which);
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-  if (key === code[index]) {
-    index++;
+function init() {
+  let index = 0;
 
-    if (index === code.length) {
-      alert("Congradulations, login successful!");
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
-  debugger
+  });
 }
-
-
-document.body.addEventListener('keydown', init)
