20a00e302a606c01e3fcf8b2ce56cc2c1712a11b
Juan David Bermudez Celedon <juanbermucele@hotmail.com>
2018-02-20 15:28:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener("keydown", e => {
+    const key = e.which || e.location || e.detail;
+    console.log("Key " + key);
+    if (code[index] === key) {
+      index++;
+      console.log("Index " + index);
+      if (index === code.length) {
+        alert("congratulations, you have inserted the konami code");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
