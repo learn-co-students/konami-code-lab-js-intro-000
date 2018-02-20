82f1be5d8dc1c8518c86f1c6c17d0a45118cd72b
Alex Segers <a.segers.dev@gmail.com>
2018-02-20 23:46:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [65, 76, 69, 88]
+var index = 0;
+function init(e) {
+  const key = parseInt(e.which);
 
-function init() {
-  // Write your JavaScript code inside the init() function
+  if (key === code[index]) {
+    index++;
 
+    if (index === code.length) {
+      alert("Congradulations, login successful!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  debugger
 }
+
+
+document.body.addEventListener('keydown', init)
