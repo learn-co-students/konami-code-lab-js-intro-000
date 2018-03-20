51ac5dbd13cfdb820be391024e6c32ae348664c1
Daniel Dunbar <danielqdunbar@gmail.com>
2018-03-20 23:16:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
-    function onKeyDownHandler(e) {
+  const main = document.body
+  main.addEventListener('keydown', function (e) {
         const key = parseInt(e.detail || e.which);
-        
+
         if (key === code[index]) {
             index++;
             console.log(key)
-            
+
             if (index === code.length) {
                 alert('Hooray!');
                 index = 0;
@@ -17,10 +18,5 @@ function init() {
         else {
             index = 0;
         }
-    }
-
-    const main = document.getElementById('main')
-
-    main.addEventListener('keydown', onKeyDownHandler(e))
+    })
 }
-
