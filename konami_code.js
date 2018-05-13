eb796851279fed39032fbf4569398cd73f4ba623
Bas K <b_kalkman@hotmail.com>
2018-05-13 09:07:25 +0000

    Done

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  var counter = 0
+
+
+  document.body.addEventListener('keydown', function(e) {
+    var key = e.key;
+    if (key === codes[counter]) {
+      counter++
+      if (counter === codes.length) {
+        alert('Hurray!')
+        counter = 0
+      }
+    } else {
+      counter = 0
+    }
+
+  })
 }
