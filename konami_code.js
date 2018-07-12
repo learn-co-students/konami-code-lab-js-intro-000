93a520561ce5cc1fbb439056cd09f902aa3fcf38
Jason Viger <jason.viger@gmail.com>
2018-07-12 07:31:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+var keys = [];
+const body = document.body;
 function init() {
   // your code here
-}
+  body.addEventListener('keydown', function(event) {
+  keys.push(event.key);
+  for(let i = 0; i< keys.length; i++){
+  if(keys[i] != codes[i]){
+    keys = [];
+  }
+ 
+  }
+ if(keys.length == codes.length){
+   alert('Hurray!');
+  }
+ 
+})
+
+  }
