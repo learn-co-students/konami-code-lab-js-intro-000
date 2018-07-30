cfeb86850b42506a173511fa5ba43ffb8f43a30f
Jonathan Hosein <jonathan.hosein@live.com>
2018-07-30 02:02:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,26 @@ const codes = [
   "b",
   "a"
 ];
+  let index = 0;
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', keydownHandler(event));
+}
+
+function keydownHandler(e) {
+  let key = e.key;
+  console.log(key);
+
+  if(e.key === codes[index]) {
+    index++;
+    if(index === codes.length)
+    {
+      alert("Hurray!");
+      index = 0;
+    }
+  }
+  else {
+    index = 0;
+  }
 }
