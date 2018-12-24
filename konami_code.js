490bb5c56b1b7eb04c8e3e1c7947b41b336e88f4
James Dorr <jamesdorr3@gmail.com>
2018-12-24 16:54:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,18 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+const body = document.querySelector('body')
+let index = 0
+function init(e){
+  const key = e.key;
+  if (key === codes[index]){
+    index++;
+    if (index === codes.length){
+      alert('Congratulations!')
+      index = 0
+    }
+  }else{
+    index = 0
+  }
 }
+body.addEventListener('keydown', init)
