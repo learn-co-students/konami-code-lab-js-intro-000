833afe7a3cdcc5c26e0fabc67f2bfefe659d0d62
Andreas P. Gortsilas <apgortsilas@aggate.gr>
2019-01-15 18:49:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+var iPos = 0;
+
+function funcHandleKeyDown(event) {
+  if (event.key === codes[iPos]) {
+    if (iPos === codes.length-1) {
+      alert('Congratulations!');
+      iPos = 0;
+    }
+    else {
+      iPos++;
+    }
+  }
+  else {
+    iPos = 0;
+  }
+}
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', funcHandleKeyDown);
 }
