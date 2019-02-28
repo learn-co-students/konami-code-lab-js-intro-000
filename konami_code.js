87e3f0bd46f1aa0c4d39369666e9649ae6d1648f
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-28 02:02:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+// Keep track of index outside of the event handler.
+let index = 0;
+
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = e.key;
+  if (key === codes[index]) {
+    console.log(key + " pressed.");
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    console.log(key + " wrongly pressed.");
+    alert("Wrong sequence!");
+    index = 0;
+  }
+}
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown',onKeyDownHandler);
 }
