d94e8594359b7087b7416171db0ef55777b9335e
Tim Golding <tgolding55@hotmail.com>
2019-03-14 03:56:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,23 @@ const codes = [
   "b",
   "a"
 ];
+var codeCounter = 0
 
 function init() {
-  // your code here
+document.body.addEventListener('keydown', onKeyDownHandler)
 }
+
+  function onKeyDownHandler(e)
+  {
+  const key = e.key
+  if (key === codes[codeCounter])
+  {
+    codeCounter ++
+    if (codeCounter === codes.length-1)
+    {
+      alert("Code true")
+      console.log("true")
+    }
+  } else
+    codeCounter = 0
+  }
