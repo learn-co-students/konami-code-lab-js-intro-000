c631eb00fb896f7888b4b28193d9f1d7c57de507
Nic Sevic <nasevic@gmail.com>
2019-03-31 21:52:47 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
 function init() {
-  // your code here
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', codeTest)
+}
+
+function codeTest(e) {
+  let key = e.key
+  if (key === codes[index]) {
+    index++
+  }
+  else {
+    index = 0
+  }
+  if (index === codes.length) {
+    alert('Game Unlocked')
+    index = 0
+  }
+
 }
