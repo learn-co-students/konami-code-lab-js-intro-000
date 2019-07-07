1edd7937fe83009942846820a816f6df15ddc1ee
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 11:31:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+
+  let i = 0
+
+  document.body.addEventListener('keydown', (e) => {
+    var pressed = e.key
+    if(pressed == codes[i]) {
+      i++
+    } else {
+      i = 0
+    }
+
+    if(i == 10) {
+      alert('Congratulations, you have unlocked the Konami Code!')
+      i = 0
+    }
+  })
 }
