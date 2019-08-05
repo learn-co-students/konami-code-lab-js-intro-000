499f36721856ef0902d433cdcf020df9a791e442
Nobuhiko Itakura <n.itakura22@gmail.com>
2019-08-05 09:44:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', checkCodes)
+}
+
+function checkCodes(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++
+  } else {
+    index = 0;
+  }
+
+  if (index === codes.length) {
+    alert('POWERUP');
+
+    index = 0;
+  }
 }
