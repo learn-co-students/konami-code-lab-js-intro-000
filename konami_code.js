5292c5068d8c341087f103e58cbc66619e1822fd
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-05 00:15:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,24 @@ const codes = [
   "b",
   "a"
 ];
+// Keep track of index outside of the event handler.
+let index = 0;
 
-function init() {
-  // your code here
+// This is the function that would be invoked by the event listener.
+function init(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("You did it successfully!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+document.querySelectorAll('body')[0].addEventListener('keydown', init)
