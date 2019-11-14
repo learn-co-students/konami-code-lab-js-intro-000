e9d5f977c58e94e74826a0382e13eb31567f1f4f
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-14 12:52:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,15 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener("keydown", function onKeyDownHandler(e){
+    let key = e.key;
+    if (key === codes[index]) {
+        index++;
+        if (index === codes.length) {
+          alert("Congrats!")
+          index = 0;
+        }
+    } else index = 0;
+  })
 }
