19ffb3cad9078e951d014250d28c8d7eea73823b
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-04 06:10:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+function init(){
+let index = 0;
+
+
+document.body.addEventListener('keydown', function(event){
+// your code here
+
+
+if (event.key === codes[index]) {
+  index++;
+
+  if (index === codes.length) {
+    alert("Congratualtions!");
+
+    index = 0;
+  }
+
+} else {
+  index = 0;
+}
+
+
+})
 }
