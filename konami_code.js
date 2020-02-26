f6e02fcb0b0f75ec163d535db2d5357a9d687944
Pratik <pr.tikgohil@gmail.com>
2020-02-26 10:20:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,14 @@ const codes = [
 ];
 
 function init() {
+  let pressed = []
   // your code here
+  document.body.addEventListener("keydown", (event) => {
+    pressed.push(event.key)
+    pressed.splice(-codes.length - 1,pressed.length-codes.length)
+    console.log(pressed);
+    if (pressed.toString() === codes.toString()) {
+      alert("Konami Code!")
+    }
+  })
 }
