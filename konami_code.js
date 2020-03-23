cc93b2ea00e1bb99c907fce12136bd5b7a398c90
Ben Anderson <andersjbe@gmail.com>
2020-03-23 17:36:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+var i = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener("keydown", e => {
+    var key = e.key;
+    if (key === codes[i]) {
+      i++;
+      if(i === codes.length) {
+        alert('Code confirmed');
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
