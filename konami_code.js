dafa2e843bbdecd90f0f397d75f3432ceeb2a4bf
Kevin Minutti <minutti70@gmail.com>
2020-03-29 22:38:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,19 +11,20 @@ const codes = [
   "a"
 ];
 
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
 
 function init() {
-  var i = 0;
-document.addEventListener('keydown', function (e) {
-  const key = parseInt(e.detail || e.which);
+  let index = 0
+document.body.addEventListener("keydown", (event) => {
+  const key = event.key;
 
-  if (key === code[i]) {
-    i++;
+  if (key === codes[index]) {
+    index++;
 
-  if (index === code.length - 1) {
-    window.alert("Congrats!");
-    i =0;}
-} else { i = 0; }
-}, false);
+    if (index === codes.length) {
+      alert("Cool!");
+      index = 0;}
+   } else {
+     index = 0;}
+ });
 }
