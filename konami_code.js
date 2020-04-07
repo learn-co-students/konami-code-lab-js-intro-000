88007d42499bd8154b3ad6496ddd060ba33bc16e
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-07 14:25:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,29 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+// function sequenceChecker() {
+//     const keyPressed = e.key;
+//
+//     if (keyPressed === codes[index]) {
+//       index++
+//
+//       if (index === codes.length) {
+//         alert("Nice one!");
+//       }
+//     }
+//   }
+
+  function init() {
+    let index = 0
+    document.body.addEventListener("keydown", function(e) {
+      const keyPressed = e.key;
+
+      if (keyPressed === codes[index]) {
+        index++
+
+        if (index === codes.length) {
+          alert("Nice one!");
+    }
+  }
+})
 }
