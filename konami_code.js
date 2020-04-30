ab9f13c9888fcb36fc079dc8bbb16b03126125e8
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-30 18:38:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,27 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0
 
-function init() {
+function init()
+{
   // your code here
+  document.body.addEventListener("keydown", (event) => {
+
+    const key = event.key;
+    if (key == codes[index])
+    {
+      index++;
+      if (index == codes.length)
+      {
+        alert(`W0W!!! U R L33T H4K3R!!! 1MPR3551V3!!!(Hurray)`)
+        index = 0;
+      }
+    }
+    else
+    {
+      console.log(`nope`)
+      index = 0;
+    }
+  })
 }
