47a5497bb1dbd215c7dd41e806c293d26edbdd5b
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-06 14:37:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index=0;
+  document.body.addEventListener("keydown", (event) => {
+    const key = event.key
+
+    if (codes[index] == key) {
+      index++
+    }
+
+    else {
+      index = 0
+    }
+
+    if (index == 10) {
+      alert("Hurray!");
+      index = 0
+    }
+  });
+
 }
