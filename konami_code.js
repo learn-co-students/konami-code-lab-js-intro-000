7431e515602c7b3f36b50af8a2fe71d9887542e0
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 05:11:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,21 @@ const codes = [
   "b",
   "a"
 ];
+var correctSeq = [];
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', event => {
+    correctSeq.push(event.key)
+
+    for (let i in correctSeq){
+      if (correctSeq[i] !== codes[i]){
+        correctSeq = []
+      }
+    }
+
+    if (correctSeq.length === codes.length){
+      alert('Hurray')
+      correctSeq = []
+    }
+    })
 }
