a2c6b2f568dfbc7dee323773c8556f137f9064f2
Amy Simpson <simpson.r.amy@gmail.com>
2020-07-15 18:30:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,36 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+ let index = 0;
+  // Attaching an keydown event listener to document.body
+  document.body.addEventListener("keydown", (event) => {
+    const key = e.key;
+  if (codes[index] === key) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  
+  
+  });
 }
+
+/*
+document.body.addEventListener('keydown', init);
+
+function init(e) {
+  newCodeArray = [];
+  newCodeArray.push(e);
+  
+  if(codes[i] === newCodeArray[i]){
+    alert("correct order"); 
+  } else {
+    newCodeArray = [];
+  }
+}
+*/
