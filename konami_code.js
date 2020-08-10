6d00dd2350cbd9d8803b627facabf9fa5ecc9440
Minh Vu <mvu7@uic.edu>
2020-08-10 18:34:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+let i = 0;
+let check = true;
 function init() {
   // your code here
-}
+  document.body.addEventListener("keydown", event =>{
+    const key = event.key;
+    if (key != codes[i]){
+      check = false;
+      i++;
+    }
+    else if(key == codes[i]){
+      i++;
+    }
+    if (check == true && i == codes.length ){
+      alert("Hurray!");
+    }
+  });
+};
