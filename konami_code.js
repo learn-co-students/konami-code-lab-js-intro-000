86d296b95c6779504881e2e2a2130f12e1d0ac0b
Robert Killion <robertfkillion@gmail.com>
2021-05-08 15:50:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,18 @@ const codes = [
   "a"
 ];
 
+let keyLog = [];
+let reference = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key===codes[reference]) {
+      keyLog.push(e.key);
+      reference++;
+      if (keyLog.length===codes.length) {alert("Hurray!")};
+    } else {
+      reference = 0
+      keyLog = [];
+    }});
 }
