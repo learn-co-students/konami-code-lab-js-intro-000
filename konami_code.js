8a150f015175ad2e227533d2639d2980a2ce3f94
Daniel Calvin <arockne2003@yahoo.com>
2021-05-22 15:54:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', event => {
+    event.key === codes[index] ? index += 1 : index = 0;
+    if (index === codes.length) {
+      alert('Hurray')
+      index = 0;
+    }
+  });
+  
+  //if key is equal to codes at index
+    //increment index
+  //otherwise
+    //reset index to 0
+  //if the length of codes is equal to index
+    //alert the user Congratulations!
 }
